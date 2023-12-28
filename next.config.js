ab4b85e9444b1199acda1c72b3655b0848dc63c7
Houssem Saïah <houcemesaiah@gmail.com>
2023-12-28 14:43:58 -0500

    Songs fetching and list display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains: [
+            "cphjbaxivvqiieabsmfj.supabase.co"
+        ]
+    }
+}
 
 module.exports = nextConfig
